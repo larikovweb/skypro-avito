b0278da379ca274118ed09dd69244d6d42b4a89c
larikovweb <larikov.web@gmail.com>
2023-12-06 18:02:26 +0300

    fix: remove login page

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,5 @@
 // ROUTES
 export const MAIN_ROUTE = '/';
-export const LOGIN_ROUTE = '/login';
-export const REGISTRATION_ROUTE = '/registration';
 export const PROFILE_ROUTE = '/profile';
 export const ARTICLE_ROUTE = '/article';
 export const MY_ARTICLE_ROUTE = '/my-article';
